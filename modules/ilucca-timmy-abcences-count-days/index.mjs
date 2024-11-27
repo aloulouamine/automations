bedc3c6242f0e9901dd59c9aca89d195a32d7dcb
Amine ALOULOU <amine.aloulou.tn@gmail.com>
2024-11-27 22:50:09 +0100

    feat(ilucca days): save planning data to JSON file

--- modules/ilucca-timmy-abcences-count-days/index.mjs
+++ modules/ilucca-timmy-abcences-count-days/index.mjs
@@ -1,13 +1,16 @@
 import { chromium } from "playwright";
 import { config } from "dotenv";
 import { IluccaPlanning } from "./ilucca-planning.mjs";
+import { writeFileSync } from "fs";
 config();
 
 (async () => {
   const browser = await chromium.launch({
-    headless: false,
+    headless: true,
   });
-  const context = await browser.newContext();
+  const context = await browser.newContext({
+    locale: "fr-FR",
+  });
   const page = await context.newPage();
   await page.goto(
     `https://${process.env.ILUCCA_ORGANIZATION}.ilucca.net/identity/login`
@@ -17,15 +20,21 @@ config();
   await page.getByPlaceholder("Mot de passe").fill(process.env.ILUCCA_PASSWORD);
   await page.getByRole("button", { name: "Se connecter" }).click();
 
-  const responsePromise = page.waitForResponse(
-    `**/DemandePlanning.ashx**`)
+  const responsePromise = page.waitForResponse(`**/DemandePlanning.ashx**`);
 
   await page.getByRole("link", { name: "Timmi Absences" }).click();
   const response = await responsePromise;
-  const responseJson = await response.json(); 
+  const responseJson = await response.json();
   const planning = new IluccaPlanning(responseJson.planning);
-  console.log(`prensece days : ${planning.countPresenceDays()}`);
-
+  // save to file
+  writeFileSync(
+    "./output/ilucca-planning.json",
+    JSON.stringify(
+      Object.fromEntries(planning.getMonthPresenceDaysMap()),
+      null,
+      2
+    )
+  );
   // ---------------------
   await context.close();
   await browser.close();
