b9995dea3e01fff65b0935d98832315e00a093fd
Amine ALOULOU <amine.aloulou.tn@gmail.com>
2025-04-21 21:39:33 +0200

    fix: login click method

--- modules/free-box-dl-last-invoice/index.js
+++ modules/free-box-dl-last-invoice/index.js
@@ -5,12 +5,16 @@ require('dotenv').config();
 
 
 (async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch(
+    {
+      headless: true,
+    }
+  );
   const page = await browser.newPage();
   await page.goto("https://subscribe.free.fr/login/");
   await page.fill('input[name="login"]', process.env.FREE_BOX_LOGIN);
   await page.fill('input[name="pass"]',  process.env.FREE_BOX_PASSWORD);
-  await page.click('input[type="submit"]');
+  await page.getByLabel('Identifiant').click();
   const downloadPromise = page.waitForEvent("download");
   await page.click(
     "#widget_mesfactures > div.widget_content > div > ul > li:nth-child(1) > a"
